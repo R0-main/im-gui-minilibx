7e53ba743ba78edf56c0fa26d8ed9d4e3d669494
R0-main <justromixx@gmail.com>
2025-04-28 16:00:57 +0200

    fwqfqfq

---
+++ src/shapes/circle.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   circle.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rguigneb <rguigneb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 14:16:50 by rguigneb          #+#    #+#             */
+/*   Updated: 2025/04/28 15:16:48 by rguigneb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "intern.h"
+
+void imglx_draw_circle(t_img *buffer, t_uvec_2 pos, size_t r)
+{
+	// size_t t1 = r / 16;
+	// size_t x = r;
+	// size_t y = 0;
+
+	// while (x >= y)
+	// {
+	// 	igmlx_put_pixel(buffer, (t_uvec_2){pos.x + x, pos.y + y}, )
+	// }
+	(void)buffer, (void)pos, (void)r;
+	_error("not implemented");
+}
